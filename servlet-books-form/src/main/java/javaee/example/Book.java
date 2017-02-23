3a1d77c99d289a056ed5574bf1fc428cda503da9
Radu Dumbraveanu <rdumbraveanu@amsoft-group.com>
2017-02-23 16:44:51 +0200

    Proiectul 'servlet-books-form' (fara a fi verificat)

---
+++ servlet-books-form/src/main/java/javaee/example/Book.java
@@ -0,0 +1,54 @@
+package javaee.example;
+
+import java.net.URL;
+
+/**
+ * Created by Radu Dumbrăveanu on 2/10/17.
+ */
+public class Book {
+    private Long id;
+    private String title;
+    private String[] authors;
+    private Integer editionNumber;
+    private URL url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+
+    public Integer getEditionNumber() {
+        return editionNumber;
+    }
+
+    public void setEditionNumber(Integer editionNumber) {
+        this.editionNumber = editionNumber;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+}
--- servlet-books/.idea/sonarlint/issuestore/index.pb
+++ servlet-books/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,20 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+V
+&src\main\java\javaee\example\Book.java,6\4\645b2782b7051a7d02ba4ae356d589881e4767d5
+O
+src\main\webapp\WEB-INF\web.xml,c\f\cfe67fbb2b19ed4a1292f6d91e148a922cf71401
+I
+src\main\webapp\books.jsp,c\9\c9f3903dd8780eed3cf224b41a06728820b40ae2
+^
+..idea\artifacts\servlet_books_war_exploded.xml,0\2\020663f2e7fb6410ea80b787af6801009a009446
+U
+%.idea\artifacts\servlet_books_war.xml,9\0\90d34b0c3ec5090d639b9c0b063fea860b8f3626
+A
+servlet-books.iml,4\b\4bdc32d8b3fa2f7db32f305e5ab15966665b3c07
+]
+-src\main\java\javaee\example\BookServlet.java,6\3\63e225aa4380aeb6b571e0b7f1c42630e98b9e80
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
